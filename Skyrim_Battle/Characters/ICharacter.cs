0368e2f99e2960defa8c741ea85e5fe15e86c544
BaBishop1 <Babishop489@gmail.com>
2023-03-13 13:03:26 -0400

    Interface Done

---
+++ Skyrim_Battle/Characters/ICharacter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skyrim_Battle.Characters
+{
+    public interface ICharacter
+    {
+        int Health{get;}
+        Attack Attack();
+        public string TakeDamage(Attack attack);
+    }
+
+    public enum AttackType{Slashing, Magic, Ranged}
+
+    public class Attack
+    {
+        public string Name{get;}
+        public int Damage{get;}
+        public AttackType Type{get;}
+
+        public Attack(string name, int damage, AttackType type)
+        {
+            Name = name;
+            Damage = damage;
+            Type = type;
+        }
+    }
+
+    
+}
---
+++ Skyrim_Battle/Characters/Nord.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skyrim_Battle.Characters
+{
+    public class Nord : ICharacter
+    {
+        public int Heath {get; private set;} = 110;
+
+        public Attack Attack()
+        {
+            Random rng = newRandom();
+            if (rng.Next(0,3) == 0)
+            {
+                return new Attack("Sword Slash", 10, AttackType.Slashing);
+            }
+            else if(rng.Next(0,3) == 1)
+            {
+                return new Attack("Bow Shot", 10, AttackType.Ranged);
+            }
+            else
+            {
+                return new Attack("Fire Bolt", 10, AttackType.Magic);
+            }
+        }
+
+        public string TakeDamage(Attack attack)
+        {
+            if (attack.Type == AttackType.Magic)
+            {
+                Health -= (attack.Damage + 3);
+            }
+            else if(attack.Type == AttackType.Slashing)
+            {
+                Health -= (attack.Damage - 3);
+            }
+            else
+            {
+                Health -= (attack.Damage);
+            }
+            return "Ahhh, I'll Kill You";
+        }
+    }
+}
